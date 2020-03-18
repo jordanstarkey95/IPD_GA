1d80a03860b9298712807c577ba99d260d556268
Sven <thijssen_sven@hotmail.com>
2020-03-18 17:37:39 -0400

    experiment 1 and 2

--- cap5512.code/Experiment1.java
+++ cap5512.code/Experiment1.java
@@ -4,6 +4,7 @@ public class Experiment1
 {
     public static void main(String[] args) throws java.io.IOException
     {
+
         int[] memoryLengths = new int[]{1, 2, 3, 4, 5, 6};
         for (int memoryLength : memoryLengths)
         {
@@ -11,7 +12,7 @@ public class Experiment1
                     10, 100, 100,
                     2, 0,
                     1, 0.8, 1, 0.005,
-                    75982, 64, 1, memoryLength);
+                    75982, 1, 64, memoryLength);
 
             Search.main(new String[]{});
         }
---
+++ cap5512.code/Experiment2.java
@@ -0,0 +1,28 @@
+public class Experiment2
+{
+    public static void main(String[] args) throws java.io.IOException
+    {
+        double[] crossoverRates = new double[]{0.5, 0.6, 0.7, 0.8, 0.9};
+        double[] mutationRates = new double[]{0.001, 0.002, 0.003, 0.004, 0.005, 0.010};
+        int[] memoryLengths = new int[]{1, 2, 3, 4, 5, 6};
+        int[] numberOfGames = new int[]{32, 64};
+
+        for(double crossoverRate: crossoverRates)
+        {
+            for(double mutationRate: mutationRates)
+            {
+                for (int memoryLength : memoryLengths)
+                {
+                    for (int n: numberOfGames){
+                        new Parameters("experiment-2crossover_rate-" + crossoverRate + "mutation_rate-" + mutationRate + "memory_length-" + memoryLength + "number_of_games-" + n, "IPD", "NA",
+                            10, 100, 100,
+                            2, 0,
+                            1, crossoverRate, 1, mutationRate,
+                            75982, 1, n, memoryLength);
+                        Search.main(new String[]{});
+                }
+                }
+            }
+        }
+    }
+}
