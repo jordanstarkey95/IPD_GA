78991e0c89d9066981721a7dd478c2dfb9b03b7b
Sven <thijssen_sven@hotmail.com>
2020-03-23 10:58:46 -0400

    bug fix: memory needed to be reset (amlan)

--- cap5512.code/IPDStrategy.java
+++ cap5512.code/IPDStrategy.java
@@ -46,12 +46,9 @@ public class IPDStrategy extends FitnessFunction
         member[index].rawFitness = 0;
         Strategy player1, player2;
         IteratedPD ipd;
-        player1 = new StrategySample(member[index].chromo);
         for (int i = 0; i < Parameters.popSize; i++)
         {
-            if(i == index)
-                continue;
-            
+            player1 = new StrategySample(member[index].chromo);
             player2 = new StrategySample(member[i].chromo);
             ipd = new IteratedPD(player1, player2);
 
